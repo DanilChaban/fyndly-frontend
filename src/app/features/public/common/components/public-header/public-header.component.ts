269c79bcfe0773af83a338ee1131f10936c586b6
Danylo Chaban <krigerblood@gmail.com>
2026-05-09 21:27:43 +0300

    add: header styles, routerLink, and link color updates in public layout components

--- src/app/features/public/common/components/public-header/public-header.component.ts
+++ src/app/features/public/common/components/public-header/public-header.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-public-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './public-header.component.html',
   styleUrl: './public-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
