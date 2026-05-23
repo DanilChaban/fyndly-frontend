9b14192f8a64700a2abf3e64fff329e93d6e7866
Danylo Chaban <krigerblood@gmail.com>
2026-05-23 14:59:22 +0300

    update: integrate localization keys into email verification components and add i18n support

--- src/app/features/public/components/auth/components/auth-email-verification/components/auth-email-varification-notice/auth-email-varification-notice.component.ts
+++ src/app/features/public/components/auth/components/auth-email-verification/components/auth-email-varification-notice/auth-email-varification-notice.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-auth-email-varification-notice',
-  imports: [MatIcon],
+  imports: [MatIcon, TranslocoPipe],
   templateUrl: './auth-email-varification-notice.component.html',
   styleUrl: './auth-email-varification-notice.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
